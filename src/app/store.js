fc503e84d1b293ccd01e1a6031fa82d4212a113c
ZorroGuy <143689351+ZorroGuy@users.noreply.github.com>
2023-09-07 12:26:17 +0530

    1st commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../folder/PatientSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfiger = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfiger, reducer);
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;
--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import App from "./App";
-
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+let persistor = persistStore(store);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
